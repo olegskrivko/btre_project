3a55672256a91569c13c1d21181c9272e512f072
Olegs <olegs.krivko@gmail.com>
2024-01-03 00:21:49 +0200

    add static

--- btre/urls.py
+++ btre/urls.py
@@ -19,18 +19,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 #original
-urlpatterns = [
-    path('', include('pages.urls')),
-    path('listings/', include('listings.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('contacts/', include('contacts.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 # urlpatterns = [
 #     path('', include('pages.urls')),
 #     path('listings/', include('listings.urls')),
 #     path('accounts/', include('accounts.urls')),
 #     path('contacts/', include('contacts.urls')),
 #     path('admin/', admin.site.urls),
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
