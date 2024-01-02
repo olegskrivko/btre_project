8e7f2baea4ef9e15d6fe492ebb15a7ea5e2114a8
Olegs <olegs.krivko@gmail.com>
2024-01-02 23:57:12 +0200

    add host

--- btre/settings.py
+++ btre/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0k7(+a6m$sng+!gz=h3cyif1^6p(b35x(wxeii6f90m-grxobm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
