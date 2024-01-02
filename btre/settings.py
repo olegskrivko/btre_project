5ecfaba1a7c2bf990cf0759869eb9cc9c55521de
Olegs <olegs.krivko@gmail.com>
2024-01-02 21:13:40 +0200

    add vercel config

--- btre/settings.py
+++ btre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0k7(+a6m$sng+!gz=h3cyif1^6p(b35x(wxeii6f90m-grxobm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
